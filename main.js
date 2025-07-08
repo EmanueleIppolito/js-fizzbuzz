32977137ecb8444a8555cfbef15eaa393cdf503b
EmanueleIppolito <emanuele2410@gmail.com>
2025-07-08 21:36:50 +0200

    End exercise

--- main.js
+++ main.js
@@ -16,5 +16,16 @@
 //Scrivere FizzBuzz
 
 for (let i = 1; i <= 100; i++){
-    
+    if (i % 3 === 0 && i % 5 === 0){
+        document.writeln("FizzBuzz");
+    }
+    else if (i % 3 === 0){
+        document.writeln("Fizz");
+    }
+    else if (i % 5 === 0){
+        document.writeln("Buzz");
+    }
+    else{
+        document.writeln(i);
+    }
 }
